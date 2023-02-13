fc9d819e731bd78274c2f72f23d732dda4c4d809
ErnesN <106961712+ErnesN@users.noreply.github.com>
2023-02-13 22:30:09 +0000

    Deploying to gh-pages from @ ErnesN/goit-react-hw-05-movies@4fc07f98a32d7d4341a1346791e3e40adf0583c1 🚀

---
+++ static/js/667.d20ddc30.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[667],{667:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"Movies page"})}}}]);
+//# sourceMappingURL=667.d20ddc30.chunk.js.map
---
+++ static/js/794.ca9154ce.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{794:function(e,t,r){r.r(t);var n=r(87),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h2",{children:"Page not found"}),(0,o.jsx)(n.rU,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=794.ca9154ce.chunk.js.map
